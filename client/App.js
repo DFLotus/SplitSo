09e311f8822ac3156956e432539d5fcb47ded7f2
DFLotus <deondraefrancis101@gmail.com>
2023-12-26 16:48:30 -0500

    SPLIT-1: Created app header

--- client/App.js
+++ client/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text, useColorScheme, View, } from 'react-native';
+import Header from './src/components/Header';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <Text>Hello, World</Text>
+    <View>
+      <Header/>
+      <Text>Hello, World</Text>
+    </View>
   );
 }
 
