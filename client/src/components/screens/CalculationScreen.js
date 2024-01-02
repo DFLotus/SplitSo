81749b2db8d2f46599c718b0175188877cc9f251
DFLotus <deondraefrancis101@gmail.com>
2024-01-02 16:08:55 -0500

    SPLIT-2: Added tip and split options

--- client/src/components/screens/CalculationScreen.js
+++ client/src/components/screens/CalculationScreen.js
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
 import { View, Text, TextInput, Alert, Button, Keyboard } from "react-native";
 import { API_KEY } from '@env'
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6"
 import calculationStyles from "../../styles/calculationStyles";
 import Header from "../Header";
 
@@ -8,11 +9,14 @@ const CalculationScreen = () => {
     const [zip_code, setZipCode] = useState(null);
     const [cost, setCost] = useState(null);
     const [split, setSplitAmount] = useState(null);
+    const [tip, setTipAmount] = useState(null);
+    const [share, setSharingOption] = useState(false);
     const [total, setTotal] = useState(0);
+    const [totalSplit, setTotalSplit] = useState(0);
 
     const calculateTotal = async () => {
         try {
-            if (!(cost) || !(zip_code)) {
+            if (!(cost) || !(zip_code) || (share && !(tip) || !(split))) {
                 Alert.alert("Input Validation", "Please enter valid amount for field(s)");
                 return;
             }
@@ -30,7 +34,11 @@ const CalculationScreen = () => {
                 const res = await response.json();
                 const taxRate = parseFloat(res[0].total_rate);
                 const taxAmount = (cost) * taxRate;
-                setTotal(parseFloat(cost) + taxAmount);
+                setTotal((parseFloat(cost) + taxAmount).toFixed(2));
+                if (share) {
+                    setTotalSplit(((parseFloat(cost) + taxAmount + tip) / split).toFixed(2));
+                };
+
                 Keyboard.dismiss();
             }
             else {
@@ -43,11 +51,16 @@ const CalculationScreen = () => {
         };
     };
 
+    const handleSharingOption = () => {
+        setSharingOption(!share);
+    };
+
     return (
         <View>
             <Header />
             <View>
                 <Text style={calculationStyles.titleText}>Calculation</Text>
+                <FontAwesome6 name={"people-group"} size={25} color={"#3498db"} onPress={handleSharingOption} />
                 <TextInput
                     placeholder="Meal cost"
                     keyboardType="numeric"
@@ -60,7 +73,24 @@ const CalculationScreen = () => {
                     value={zip_code}
                     onChangeText={(value) => setZipCode(value)}
                 />
+                {share &&
+                    <>
+                        <TextInput
+                            placeholder="Tip amount"
+                            keyboardType="numeric"
+                            value={tip}
+                            onChangeText={(value) => setTipAmount(value)}
+                        />
+                        <TextInput
+                            placeholder="Number of people including self"
+                            keyboardType="numeric"
+                            value={split}
+                            onChangeText={(value) => setSplitAmount(value)}
+                        />
+                    </>
+                }
                 <Text>Total Cost: {total}</Text>
+                {share && <><Text>Total Split Amount: {totalSplit}</Text></>}
                 <Button title="Calculate Total" onPress={calculateTotal} />
             </View>
         </View>
